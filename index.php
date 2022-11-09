30c2509bc2afb6c1993876f6a0d57d476c396899
Ameer-Ali-Jamali <ameer.ali.jamali886@gmail.com>
2022-11-09 19:20:04 +0500

    now easy to read

--- index.php
+++ index.php
@@ -1,59 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+Objects
+Object is an instance of class. An Object is a self-contained component which consists of methods and properties to make a particular type of data useful. We can create multiple objects from a class and each object will have all the properties and methods defined in the class, but they will have different property values. To declare an object in PHP we use the new keyword.
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+Example:
 
-<body>
-    <?php
+$object_name = new ClassName();
 
 
-    // Objects
-    // Object is an instance of class. An Object is a self-contained component which consists of methods and properties to make a particular type of data useful. We can create multiple objects from a class and each object will have all the properties and methods defined in the class, but they will have different property values. To declare an object in PHP we use the new keyword. 
+Creating an Object in PHP
+We will create two objects in the following example.
 
-    // Example:
+<?php
+class Employee
+{
+    // Properties
+    public $name;
+    public $surname;
 
-    // $object_name = new ClassName();
-
-
-    // Creating an Object in PHP
-    // We will create two objects in the following example. 
-
-    class Employee
+    // Methods
+    function set_name($name)
     {
-        // Properties
-        public $name;
-        public $surname;
-
-        // Methods
-        function set_name($name)
-        {
-            $this->name = $name;
-        }
-        function get_name()
-        {
-            return $this->name;
-        }
+        $this->name = $name;
     }
+    function get_name()
+    {
+        return $this->name;
+    }
+}
 
-    $emp1 = new Employee();
-    $emp2 = new Employee();
-    $emp1->set_name('Anwar');
-    $emp2->set_name('Shuaib');
+$emp1 = new Employee();
+$emp2 = new Employee();
+$emp1->set_name('muneeb');
+$emp2->set_name('akram');
 
-    echo $emp1->get_name();
-    echo "<br>";
-    echo $emp2->get_name();
-
-    // Output:
-
-    // Anwar
-    // Shuaib
-    ?>
-</body>
-
-</html>
+echo $emp1->get_name();
+echo "<br>";
+echo $emp2->get_name();
+?>
